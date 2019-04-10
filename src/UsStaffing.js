e49dda47d21f62069b6fb6bcd189592aa56c5cc1
Alladi <170743@NTTDATA.COM>
2019-04-10 19:01:07 +0530

    it services page's background is modified, us staffing background images are updated.

--- src/UsStaffing.js
+++ src/UsStaffing.js
@@ -16,57 +16,39 @@ export default class UsStaffing extends Component {
         return (
             <div className="mainDiv" >
                 <p className="usH" >US STAFFING</p>
-                <div className="stage yellow" >
-                    <p className="paragraph" >Once we undertake US Staffing assignments from clients,
-                        we synchronize all the requirements with existing resource databases both conventional and non-conventional, referral networks, etc. in searching the right talent.
-                    </p>
-                    <div className="arrowDiv yellow" >
-                        <img className="arrowIcn" src={arrow} />
-                    </div>
-                </div>
-                <div className="stage white" >
-                    <p className="paragraph" >
-                        We prepare exclusive search strategy to address the unique needs of clients for finding the most apt talent pool of resources.
+                <div className="usMainDiv" >
+                    <div className="stepsDiv" >
+                        <p className="paragraph" >Once we undertake US Staffing assignments from clients,
+                            we synchronize all the requirements with existing resource databases both conventional and non-conventional, referral networks, etc. in searching the right talent.
+                        </p>
+                        <p className="paragraph" >
+                            We prepare exclusive search strategy to address the unique needs of clients for finding the most apt talent pool of resources.
                         <br />
-                        Based on the strategy, we search the talent and shortlist the probable resources.
-                    </p>
-                    <div className="arrowDiv white " >
-                        <img className="arrowIcn" src={arrow} />
+                            Based on the strategy, we search the talent and shortlist the probable resources.
+                        </p>
+                        <p className="paragraph" >
+                            Out of that probable list, we again scan and screen profiles based on their earlier roles and responsibilities,
+                            additional qualifications, willingness to relocate to overseas locations, etc. Thus, we prepare the final list and complete the staffing assignment.
+                        </p>
                     </div>
-                </div>
-                <div className="stage yellow" >
-                    <p className="paragraph" >
-                        Out of that probable list, we again scan and screen profiles based on their earlier roles and responsibilities,
-                        additional qualifications, willingness to relocate to overseas locations, etc. Thus, we prepare the final list and complete the staffing assignment.
-                    </p>
-                    <div className="arrowDiv yellow" >
-                        <img className="arrowIcn" src={arrow} />
+                    <div className="stepsDiv rightImg " ></div>
+                    <div className="stepsDiv leftImg" ></div>
+                    <div className="stepsDiv" >
+                        <p className="paragraph" >
+                            In all steps of our US Staffing, we accentuate on simplifying processes,
+                            procedures, strategy compliances for stakeholders like clients, associates, workforce and prospective resources.
+                        </p>
+                        <p className="paragraph" >
+                            We emphasize on clients and their unique requirements. From analysis to deployment,
+                            we complete each step in a cohesive manner with client feedback being an important part of the process.
+                        </p>
+                        <p className="paragraph" >
+                            Serving clients with the best in marketing strategies, providing high end consulting and service.
+                            Sourcing and deploying highly skilled IT specialists in mainstream and good technologies to meet client's temporary, permanent or contracting needs.
+                            We provide the best recruitment solutions for every assignment while ensuring quality, integrity, expertise at an affordable cost.
+                        </p>
                     </div>
-                </div>
-                <div className="stage white" >
-                    <p className="paragraph" >
-                        In all steps of our US Staffing, we accentuate on simplifying processes,
-                        procedures, strategy compliances for stakeholders like clients, associates, workforce and prospective resources.
-                    </p>
-                    <div className="arrowDiv white" >
-                        <img className="arrowIcn" src={arrow} />
-                    </div>
-                </div>
-                <div className="stage yellow" >
-                    <p className="paragraph" >
-                        We emphasize on clients and their unique requirements. From analysis to deployment,
-                        we complete each step in a cohesive manner with client feedback being an important part of the process.
-                    </p>
-                    <div className="arrowDiv yellow" >
-                        <img className="arrowIcn" src={arrow} />
-                    </div>
-                </div>
-                <div className="stage white" >
-                    <p className="paragraph lastP" >
-                        Serving clients with the best in marketing strategies, providing high end consulting and service.
-                        Sourcing and deploying highly skilled IT specialists in mainstream and good technologies to meet client's temporary, permanent or contracting needs.
-                        We provide the best recruitment solutions for every assignment while ensuring quality, integrity, expertise at an affordable cost.
-                    </p>
+
                 </div>
             </div>
         );
